23cf706c7e70e88b78f69da290694f901e09036b
Wayne Olson <wayne@wayneo.co>
2022-02-12 14:43:34 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Sinatra based todos app for Launch School's RB175 course.
---
+++ todo.rb
@@ -0,0 +1,89 @@
+require 'sinatra'
+require 'sinatra/content_for'
+require 'sinatra/reloader'
+require 'tilt/erubis'
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get '/' do
+  redirect '/lists'
+end
+
+# GET    /lists     -> view all lists
+# GET    /lists/new -> new list form
+# POST   /lists     -> create new list
+# GET    /lists/1   -> view a single list
+
+# view list of lists
+get '/lists' do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# render the new list form
+get '/lists/new' do
+  erb :new_list, layout: :layout
+end
+
+# return an error message if the name is invalid. Return nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    'List name must be between 1 and 100 characters.'
+  elsif session[:lists].any? { |list| list[:name] == name }
+    'List name must be unique.'
+  end
+end
+
+get '/lists/:id' do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  erb :list, layout: :layout
+end
+
+# Edit an existing todo list
+get '/lists/:id/edit' do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  erb :edit_list, layout: :layout
+end
+
+# Update an existing todo list
+post '/lists/:id' do
+  list_name = params[:list_name].strip
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = 'The list has been updated.'
+    redirect "/lists/#{id}"
+  end
+end
+
+# create a new list
+post '/lists' do
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
+  end
+end
