0a9696a058f7c3827472bcf91abd76aee02de383
Wayne Olson <wayne@wayneo.co>
2022-02-13 08:55:12 -0800

    Added 'update todo status' feature.

--- todo.rb
+++ todo.rb
@@ -128,3 +128,16 @@ post '/lists/:list_id/todos/:id/destroy' do
 
   redirect "/lists/#{@list_id}"
 end
+
+# Update status of todo
+post '/lists/:list_id/todos/:id' do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+
+  todo_id = params[:id].to_i
+  is_completed = params[:completed] == 'true'
+  @list[:todos][todo_id][:completed] = is_completed
+
+  session[:success] = 'The todo has been updated.'
+  redirect "/lists/#{@list_id}"
+end
